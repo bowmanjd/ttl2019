1017c653258d63876f73e13414b28b6b0a61b496
Jonathan Bowman <git@bowmanjd.com>
2019-05-07 08:09:55 -0400

    examples

--- command_examples.py
+++ command_examples.py
@@ -1,8 +1,25 @@
 import json
 from subprocess import check_output
 
+# https://docs.python.org/3/library/subprocess.html#subprocess.check_output
+
+# I like to pass in the command as a list of arguments,
+# which makes for easy programmatic appending or
+# slicing of args
+def show_kernel_info():
+    cmd = ["uname", "-a"]
+    output = check_output(cmd)
+    return output
+
+
+def show_ip_info():
+    cmd = ["ip", "addr"]
+    output = check_output(cmd)
+    return output
+
 
 def list_responses():
-    output = check_output(["curl", "https://ttl2019.bowmanjd.com/responses"])
+    cmd = ["curl", "https://ttl2019.bowmanjd.com/responses"]
+    output = check_output(cmd)
     responses = json.loads(output)
     return responses
